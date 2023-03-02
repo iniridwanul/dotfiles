644a7a77fa909a718aec5e5625dbcac3e7a56978
iniridwanul <theridwanul@gmail.com>
2023-03-02 12:43:09 +0600

    Added minimal startpage for Firefox

---
+++ home/iniridwanul/.modified/startpage/js/header.js
@@ -0,0 +1,73 @@
+let buttonHeader = document.getElementById('chb_header');
+let header = document.querySelector('.header');
+let headerWeather = document.querySelector('.header__weather');
+let headerGreeting = document.querySelector('.header__greeting');
+let icons = document.querySelectorAll('.main a');
+
+localStorage.getItem('header') === 'shown' || localStorage.getItem('header') === null ? showHeader() : hideHeader();
+
+buttonHeader.onclick = () => buttonHeader.checked ? showHeader() : hideHeader();
+
+function showHeader() {
+	buttonHeader.checked = true;
+	localStorage.setItem('header', 'shown');
+
+	for (let e of icons) {
+		e.classList.remove('big-icons');
+	}
+
+	header.style.opacity = '.75';
+	header.style.zIndex = '1';
+	headerWeather.classList.remove('hide-header-elements');
+	headerGreeting.classList.remove('hide-header-elements');
+}
+
+function hideHeader() {
+	buttonHeader.checked = false;
+	localStorage.setItem('header', 'hidden');
+
+	for (let e of icons) {
+		e.classList.add('big-icons');
+	}
+
+	header.style.opacity = '0';
+	header.style.zIndex = '-1';
+}
+
+document.addEventListener('keyup', (event) => {
+	let header = document.querySelector('.header');
+	let searchForm = document.querySelector('.search-form');
+	let searchInput = document.querySelector('.search-form__input');
+
+	if (event.key === 'S') {
+		if (buttonHeader.checked === false) {
+			header.style.opacity = '.75';
+			header.style.zIndex = '1';
+			headerWeather.classList.add('hide-header-elements');
+			headerGreeting.classList.add('hide-header-elements');
+
+			for (let e of icons) {
+				e.classList.remove('big-icons');
+			}
+		}
+
+		header.style.marginBottom = '35px';
+		searchForm.classList.add('show-search');
+		searchInput.focus();
+	}
+
+	if (event.code === 'Escape') {
+		if (buttonHeader.checked === false) {
+			header.style.opacity = '0';
+
+			for (let e of icons) {
+				e.classList.add('big-icons');
+			}
+		}
+
+		header.style.marginBottom = '';
+		searchForm.classList.remove('show-search');
+		searchInput.value = '';
+		searchInput.blur();
+	}
+});
---
+++ home/iniridwanul/.modified/startpage/js/weather.js
@@ -0,0 +1,104 @@
+let keyInput = document.getElementById('key');
+let cityInput = document.getElementById('city');
+let apply = document.getElementById('weather_apply');
+let weatherAlert = document.getElementById('header__weather-alert');
+let info = document.getElementById('header__weather-info');
+let radioCelsius = document.getElementById('celsius');
+let radioFahrenheit = document.getElementById('fahrenheit');
+let weatherLS = localStorage.getItem('weather');
+
+if (weatherLS !== null) {
+	let key = JSON.parse(weatherLS).key;
+	let city = JSON.parse(weatherLS).city;
+
+	keyInput.placeholder = key;
+	cityInput.placeholder = city;
+	getWeatherInfo(key, city);
+} else {
+	getWeatherInfo(null, null);
+}
+
+apply.onclick = () => {
+	let key = keyInput.value;
+	let city = cityInput.value;
+	let weatherLS = localStorage.getItem('weather');
+
+	if (key === '') {
+		if (weatherLS !== null) {
+			key = JSON.parse(weatherLS).key;
+		}
+	} else {
+		weatherAlert.style.display = 'none';
+	}
+
+	if (city === '') {
+		if (weatherLS !== null) {
+			city = JSON.parse(weatherLS).city;
+		}
+	} else {
+		weatherAlert.style.display = 'none';
+	}
+
+	radioFahrenheit.checked ? tempUnit = 'F' : tempUnit = 'C';
+
+	let info = {
+		key: key,
+		city: city,
+		tempUnit: tempUnit
+	};
+
+	localStorage.setItem('weather', JSON.stringify(info));
+
+	cityInput.value = '';
+	cityInput.blur();
+	cityInput.placeholder = city;
+
+	keyInput.value = '';
+	keyInput.blur();
+	keyInput.placeholder = key;
+
+	getWeatherInfo(key, city);
+}
+
+function getWeatherInfo(key, city) {
+	fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+		.then(function (response) {
+			return response.json();
+		})
+		.then(function (data) {
+			celsius = Math.round(parseFloat(data.main.temp) - 273.15);
+			fahrenheit = Math.round(((parseFloat(data.main.temp) - 273.15) * 1.8) + 32);
+			description = data.weather[0].description;
+		})
+		.then(function () {
+			drawWeather();
+		})
+		.catch(function (error) {
+			if (key === '' || key === null) {
+				weatherAlert.style.display = '';
+				weatherAlert.innerHTML = 'remember to add your api key';
+				keyInput.placeholder = 'Enter key';
+			} else if (city === '' || city === null) {
+				weatherAlert.style.display = '';
+				weatherAlert.innerHTML = 'remember to add city name';
+				cityInput.placeholder = 'Enter city';
+			}
+			else {
+				weatherAlert.style.display = 'none';
+				info.innerHTML = '¯\u005C_(ツ)_/¯';
+				console.log(error);
+			}
+		});
+}
+
+function drawWeather() {
+	if (JSON.parse(localStorage.getItem('weather')).tempUnit === 'C') {
+		temp = `${celsius}\u00B0C`;
+		radioCelsius.checked = true;
+	} else {
+		temp = `${fahrenheit}\u00B0F`;
+		radioFahrenheit.checked = true;
+	}
+
+	info.innerHTML = `${description}, <span class="header__weather-temp">${temp}</span>`;
+}
